5ea685f70c416ed1313d7eec282449010c46efc0
saptarshibasu15 <123saptarshi.basu@gmail.com>
2020-12-13 10:59:31 +0530

    somefixes

--- commands/repolist.command.go
+++ commands/repolist.command.go
@@ -21,18 +21,14 @@ func RepoList(owner string) {
 	displayRepoList(repo)
 }
 
-<<<<<<< HEAD
 func displayRepoList(u types.RepoList, ops ...string) {
 	fmt.Println("[")
-=======
-func displayRepoList(u types.RepoList) {
->>>>>>> 913167f96830de06a7b89167f44e53a82dfa1863
 	for _, r := range u {
 		v := reflect.ValueOf(r)
 		typeOfS := v.Type()
 		fmt.Println("\t{")
 		for i := 0; i < v.NumField(); i++ {
-			if v.Field(i).Interface() == nil || utils.Filter(typeOfS.Field(i).Name) {
+			if v.Field(i).Interface() == nil {
 				continue
 			}
 			fmt.Printf("\t\t%s : %v\n", utils.Green(typeOfS.Field(i).Name), v.Field(i).Interface())
